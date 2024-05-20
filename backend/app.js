5e7ec33d4495fbe40de673c4a94dbe61d2bd16dc
EliyasT <eliyasbirhanu1991@gmail.com>
2024-05-20 18:59:22 +0200

    Dependenies updated

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,19 @@
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
-import userRouter from './routes/userRoutes.js';
-import orderRouter from './routes/orderRoutes.js';
-import uploadRouter from './routes/uploadRoutes.js';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import uploadRouter from "./routes/uploadRoutes.js";
 
 dotenv.config();
-//password = UoYYX4PuAb32TNhd
-//mongodb+srv://admin:<password>@cluster0.pfeqpo8.mongodb.net/?retryWrites=true&w=majority
 
 mongoose
-  .connect(
-    'mongodb+srv://admin:UoYYX4PuAb32TNhd@cluster0.pfeqpo8.mongodb.net/Awash?retryWrites=true&w=majority'
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('connected to db');
+    console.log("connected to db");
   })
   .catch((err) => {
     console.log(err.message);
@@ -28,23 +24,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/keys/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
-app.get('/api/keys/google', (req, res) => {
-  res.send({ key: process.env.GOOGLE_API_KEY || '' });
+app.get("/api/keys/google", (req, res) => {
+  res.send({ key: process.env.GOOGLE_API_KEY || "" });
 });
 
-app.use('/api/upload', uploadRouter);
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
-app.use('/api/users', userRouter);
-app.use('/api/orders', orderRouter);
+app.use("/api/upload", uploadRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
 app.use((err, req, res, next) => {
